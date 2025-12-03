48399c17bd5e298cc931a59855adf81b98a8fc46
amitsharmacflms-smc <amitsharmacflms-smc.com>
2025-12-03 18:09:08 +0530

    temp sqlite for railway test

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("FLASK_SECRET_KEY", "supersecret")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
