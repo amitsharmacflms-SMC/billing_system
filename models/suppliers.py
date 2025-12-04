22b75669ebaa593c50fcbaac239752b061fbfc6d
amitsharmacflms-smc <amitsharmacflms-smc.com>
2025-12-04 18:46:46 +0530

    update

---
+++ models/suppliers.py
@@ -0,0 +1,14 @@
+from core.database import db
+from datetime import datetime
+
+class Supplier(db.Model):
+    __tablename__ = "suppliers"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    address = db.Column(db.Text)
+    gstin = db.Column(db.String(30))
+    state = db.Column(db.String(100))
+    state_code = db.Column(db.String(5))
+    phone = db.Column(db.String(30))
+    email = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
